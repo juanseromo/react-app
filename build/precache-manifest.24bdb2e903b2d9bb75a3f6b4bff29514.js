5034603be8b37816e339c0a5eda55ace9f3ee7c1
Juan Sebastian Rodriguez <juanseromo@Juans-MacBook-Pro.local>
2020-10-14 22:20:20 -0500

    d

---
+++ build/precache-manifest.24bdb2e903b2d9bb75a3f6b4bff29514.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de86f6fa29e5897b98f0fb51d5ea1459",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c2d8fe2cf745575aae41",
+    "url": "/static/css/main.d7761389.chunk.css"
+  },
+  {
+    "revision": "b83e548ef8a102d45b80",
+    "url": "/static/js/2.0516d284.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.0516d284.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2d8fe2cf745575aae41",
+    "url": "/static/js/main.396e8829.chunk.js"
+  },
+  {
+    "revision": "1ca56b6e53ce1dc83ae6",
+    "url": "/static/js/runtime-main.0a3c20bd.js"
+  }
+]);
